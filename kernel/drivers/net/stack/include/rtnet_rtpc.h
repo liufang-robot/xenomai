c2295dc7c2c173315d6d52c66c1f8dd342095777
Hannes Diethelm <hannes.diethelm@gmail.com>
2026-04-11 01:17:20 +0200

    drivers/net/stack: Migrate struct rt_proc_call to flexible array member
    This fixes the following WARNING:
    memcpy: detected field-spanning write (size 80) of single field "call->priv_data" at drivers/xenomai/net/stack/rtnet_rtpc.c:93 (size 0)
    
    Signed-off-by: Hannes Diethelm <hannes.diethelm@gmail.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- kernel/drivers/net/stack/include/rtnet_rtpc.h
+++ kernel/drivers/net/stack/include/rtnet_rtpc.h
@@ -43,7 +43,7 @@ struct rt_proc_call {
 	atomic_t ref_count;
 	wait_queue_head_t call_wq;
 	rtpc_cleanup_proc cleanup_handler;
-	char priv_data[0] __attribute__((aligned(8)));
+	char priv_data[] __attribute__((aligned(8)));
 };
 
 #define CALL_PENDING 1000 /* result value for blocked calls */
