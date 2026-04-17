liufang-robot/xenomai

commit c2b883446b8028960924137b5f6c97b0642376e6

y2038: lib/cobalt: Implement necessary wrappers for y2038 support
COBALT_IMPL_TIME64():
  This macro is based on COBALT_IMPL() and will add the necessary
  __wrap_ function as weak symbol to the cobalt implementation
  (starting with __cobalt_) when y2038 support has been requested.
  This allows the usage of the __RT() macro in combination with
  y2038/time64 wrappers.

COBALT_DECL_TIME64():
  This macro is based on COBALT_DECL() and allows the usage of __STD()
  in combination with y2038/time64 wrappers.

Both macros will fall back to their basements if y2038 support was
not requested.

Examples, assuming code is located in application code:
  select():       will be redirected by glibc to __select64() when
                  y2038 support is requested. We additionally wrap the
                  __select64() function so __wrap___select64() will be
                  called - which is a weak symbol aliased to
                  __cobalt_select(). That allows users to overwrite
                  __wrap___select64() if they need to.

  __RT(select()): result after preprocessor: __cobalt_select()
                  __cobalt_select() is y2038 safe when y2038 has been
                  requested.

Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

lib/cobalt/select.c
@@ -20,12 +20,52 @@
 #include <pthread.h>
 #include <sys/select.h>
 #include <asm/xenomai/syscall.h>
-#include "internal.h"
 
-COBALT_IMPL(int, select, (int __nfds, fd_set *__restrict __readfds,
-			  fd_set *__restrict __writefds,
-			  fd_set *__restrict __exceptfds,
-			  struct timeval *__restrict __timeout))
+#if __USE_TIME_BITS64
+/*
+ * The time64 wrapper for select() is a little different:
+ * There is no y2038 safe syscall for select() itself, but we have pselect()
+ * without signal support.
+ */
+static inline int do_select(int __nfds, fd_set *__restrict __readfds,
+			      fd_set *__restrict __writefds,
+			      fd_set *__restrict __exceptfds,
+			      struct timeval *__restrict __timeout)
+{
+	struct timespec to;
+	int err, oldtype;
+
+	if (__timeout) {
+		to.tv_sec = __timeout->tv_sec;
+		to.tv_nsec = __timeout->tv_usec * 1000;
+	}
+
+	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &oldtype);
+
+	/*
+	 * Note: No sigmask here, we already reached the limit of 5
+	 * syscall parameters
+	 */
+	err = XENOMAI_SYSCALL5(sc_cobalt_pselect64, __nfds, __readfds,
+			       __writefds, __exceptfds, __timeout ? &to : NULL);
+
+	pthread_setcanceltype(oldtype, NULL);
+
+	if (err == -EADV || err == -EPERM || err == -ENOSYS)
+		err = __STD(__select64(__nfds, __readfds, __writefds,
+				       __exceptfds, __timeout));
+
+	if (err >= 0)
+		return err;
+
+	errno = -err;
+	return -1;
+}
+#else
+static inline int do_select(int __nfds, fd_set *__restrict __readfds,
+			    fd_set *__restrict __writefds,
+			    fd_set *__restrict __exceptfds,
+			    struct timeval *__restrict __timeout)
 {
 	int err, oldtype;
 
@@ -45,4 +85,14 @@ COBALT_IMPL(int, select, (int __nfds, fd_set *__restrict __readfds,
 
 	errno = -err;
 	return -1;
+}
+#endif
+
+COBALT_IMPL_TIME64(int, select, __select64,
+		   (int __nfds, fd_set *__restrict __readfds,
+		    fd_set *__restrict __writefds,
+		    fd_set *__restrict __exceptfds,
+		    struct timeval *__restrict __timeout))
+{
+	return do_select(__nfds, __readfds, __writefds, __exceptfds, __timeout);
 }

configure.ac
@@ -944,17 +944,27 @@ dnl in-tree executables which require POSIX symbol wrapping.
 
    modechk_wrappers="$topdir/lib/cobalt/modechk.wrappers"
    cobalt_wrappers="$topdir/lib/cobalt/cobalt.wrappers"
+   cobalt_wrappers_glibc_time64="$topdir/lib/cobalt/cobalt-glibc-time64.wrappers"
+   all_wrappers="$modechk_wrappers $cobalt_wrappers"
+
+   linker_flags="-Wl,@$modechk_wrappers -Wl,@$cobalt_wrappers"
+
+   if [[ "$CONFIG_XENO_LIBS_TIME64" = "yes" ]]; then
+      all_wrappers="$all_wrappers $cobalt_wrappers_glibc_time64"
+      linker_flags="$linker_flags -Wl,@$cobalt_wrappers_glibc_time64"
+   fi
+
    if [[ $ac_cv_ld_file_option = yes ]]; then
-	XENO_POSIX_WRAPPERS="-Wl,@$modechk_wrappers -Wl,@$cobalt_wrappers"
+	XENO_POSIX_WRAPPERS="$linker_flags"
    else
-	XENO_POSIX_WRAPPERS=`cat $modechk_wrappers $cobalt_wrappers | \
+	XENO_POSIX_WRAPPERS=`cat $all_wrappers | \
 			while read wrap_option symbol ; do \
 				echo -n "-Wl,$wrap_option,$symbol " ; \
 			done`
    fi
 
    AC_SUBST(XENO_POSIX_WRAPPERS)
-   AC_SUBST([CONFIG_STATUS_DEPENDENCIES], ["$modechk_wrappers $cobalt_wrappers"])
+   AC_SUBST([CONFIG_STATUS_DEPENDENCIES], ["$all_wrappers"])
 fi
 
 dnl Multi-library support.

include/cobalt/mqueue.h
@@ -42,14 +42,14 @@ COBALT_DECL(int, mq_setattr,
 COBALT_DECL(int, mq_send,
 	    (mqd_t qd, const char *buffer, size_t len, unsigned prio));
 
-COBALT_DECL(int, mq_timedsend,
-	    (mqd_t q, const char *buffer, size_t len, unsigned prio,
-	     const struct timespec *timeout));
+COBALT_DECL_TIME64(int, mq_timedsend, __mq_timedsend64,
+		   (mqd_t q, const char *buffer, size_t len, unsigned prio,
+		    const struct timespec *timeout));
 
 COBALT_DECL(ssize_t, mq_receive,
 	    (mqd_t q, char *buffer, size_t len, unsigned *prio));
 
-COBALT_DECL(ssize_t, mq_timedreceive,
+COBALT_DECL_TIME64(ssize_t, mq_timedreceive, __mq_timedsend_time64,
 	    (mqd_t q, char *__restrict__ buffer, size_t len,
 	     unsigned *__restrict__ prio,
 	     const struct timespec *__restrict__ timeout));

include/cobalt/pthread.h
@@ -59,8 +59,8 @@ COBALT_DECL(int, pthread_mutex_destroy, (pthread_mutex_t *mutex));
 
 COBALT_DECL(int, pthread_mutex_lock, (pthread_mutex_t *mutex));
 
-COBALT_DECL(int, pthread_mutex_timedlock,
-	    (pthread_mutex_t *mutex, const struct timespec *to));
+COBALT_DECL_TIME64(int, pthread_mutex_timedlock, __pthread_mutex_timedlock64,
+		   (pthread_mutex_t *mutex, const struct timespec *to));
 
 COBALT_DECL(int, pthread_mutex_trylock, (pthread_mutex_t *mutex));
 
@@ -81,9 +81,9 @@ COBALT_DECL(int, pthread_cond_destroy, (pthread_cond_t *cond));
 COBALT_DECL(int, pthread_cond_wait,
 	    (pthread_cond_t *cond, pthread_mutex_t *mutex));
 
-COBALT_DECL(int, pthread_cond_timedwait,
-	    (pthread_cond_t *cond, pthread_mutex_t *mutex,
-	     const struct timespec *abstime));
+COBALT_DECL_TIME64(int, pthread_cond_timedwait, __pthread_cond_timedwait64,
+		   (pthread_cond_t *cond, pthread_mutex_t *mutex,
+		    const struct timespec *abstime));
 
 COBALT_DECL(int, pthread_cond_signal, (pthread_cond_t *cond));
 

include/cobalt/semaphore.h
@@ -37,8 +37,8 @@ COBALT_DECL(int, sem_post, (sem_t *sem));
 
 COBALT_DECL(int, sem_wait, (sem_t *sem));
 
-COBALT_DECL(int, sem_timedwait,
-	    (sem_t *sem, const struct timespec *abs_timeout));
+COBALT_DECL_TIME64(int, sem_timedwait, __sem_timedwait64,
+		   (sem_t *sem, const struct timespec *abs_timeout));
 
 COBALT_DECL(int, sem_trywait, (sem_t *sem));
 

include/cobalt/signal.h
@@ -46,9 +46,9 @@ COBALT_DECL(int, sigwait, (const sigset_t *set, int *sig));
 
 COBALT_DECL(int, sigwaitinfo, (const sigset_t *set, siginfo_t *si));
 
-COBALT_DECL(int, sigtimedwait,
-	    (const sigset_t *set, siginfo_t *si,
-	     const struct timespec *timeout));
+COBALT_DECL_TIME64(int, sigtimedwait, __sigtimedwait64,
+		   (const sigset_t *set, siginfo_t *si,
+		    const struct timespec *timeout));
 
 COBALT_DECL(int, kill, (pid_t pid, int sig));
 

include/cobalt/sys/ioctl.h
@@ -27,7 +27,8 @@
 extern "C" {
 #endif
 
-COBALT_DECL(int, ioctl, (int fildes, unsigned int request, ...));
+COBALT_DECL_TIME64(int, ioctl, __ioctl_time64,
+		   (int fd, unsigned int request, ...));
 
 #ifdef __cplusplus
 }

include/cobalt/sys/select.h
@@ -27,7 +27,7 @@
 extern "C" {
 #endif
 
-COBALT_DECL(int, select,
+COBALT_DECL_TIME64(int, select, __select64,
 	    (int __nfds, fd_set *__restrict __readfds,
 	     fd_set *__restrict __writefds, fd_set *__restrict __exceptfds,
 	     struct timeval *__restrict __timeout));

commit a46e7958dd6063e19a1d70367dc874ffce2e856f

y2038: lib/cobalt: select: Fix timeout writeback of the compat time64_t case
The kernel might modify the timeout parameter. As we have to convert
from struct timeval (user) to struct timespec (kernel, pselect) the
updated value was hidden from the user.

Fixes: c2b883446b80 ("y2038: lib/cobalt: Implement necessary wrappers for y2038 support")
Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

lib/cobalt/select.c
@@ -57,9 +57,13 @@ static inline int do_select(int __nfds, fd_set *__restrict __readfds,
 
 	pthread_setcanceltype(oldtype, NULL);
 
-	if (err == -EADV || err == -EPERM || err == -ENOSYS)
+	if (err == -EADV || err == -EPERM || err == -ENOSYS) {
 		err = __STD(__select64(__nfds, __readfds, __writefds,
 				       __exceptfds, __timeout));
+	} else if (__timeout) {
+		__timeout->tv_sec = to.tv_sec;
+		__timeout->tv_usec = to.tv_nsec / 1000;
+	}
 
 	if (err >= 0)
 		return err;